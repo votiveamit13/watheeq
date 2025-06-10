47dd2047ca80d3895e7e946813361fed00268894
krishna <votivedesigner.krishna@gmail.com>
2025-06-10 18:35:46 +0530

    css changes in whoweare and prices

--- src/app/whoweare/page.jsx
+++ src/app/whoweare/page.jsx
@@ -10,11 +10,11 @@ export default function WhoWeAre() {
 <div className="bg-[#f8fafc] bg-[url('/watheeq/assets/img/hero_bg.png')] bg-cover">
       <nav className="container mx-auto px-[15px] sm:px-10 p-1 px-20 text-center">
         <Navbar className="" />
-        <p className="text-3xl mt-15 mb-5">من نحن</p>
+        <p className="text-3xl mt-15 mb-5 text-[#0a84ff]">من نحن</p>
         <h2 className="text-[35px] sm:text-5xl font-bold mb-5">
         رحلتنا بدأت لخدمة المحامين ... تعرف عليها
         </h2>
-        <p className="text-2xl mb-15 text-[#0B2B51]">
+        <p className="text-2xl mb-15 text-[#13498b]">
           وُلدت وثيق من إيماننا بحاجة المحامي العصري إلى حضور رقمي موثوق وقوي .
           منصة وثيق تمكّن المحامين من بناء صفحاتهم القانونية، إدارة طلباتهم،
           واستقبال مستحقاتهم بسهولة، مع تعزيز ثقته في السوق الرقمي
@@ -35,7 +35,7 @@ export default function WhoWeAre() {
       <div className="bg-white p-[20px] sm:p-20 block sm:flex gap-4 items-center m-0 sm:m-5 shadow-xl rounded-3xl mb-[30px]">
         <div className="justify-between flex-1">
             <h3 className="text-[35px] sm:text-5xl font-bold mb-5">حكايتنا</h3>
-            <p className="text-3xl sm:text-1xl mb-5 text-[#0B2B51]">
+            <p className="text-3xl sm:text-1xl mb-5 text-[#13498b]">
               منصة " وثيق " منصة إلكترونية مبتكرة مخصصة للمحامين المرخّصين .
               توفر المنصة أدوات رقمية لإدارة الطلبات القانونية، الفواتير ،
               التواصل مع العملاء، وتنظيم الخدمات . كل هذا في بيئة قانونية موثوقة
