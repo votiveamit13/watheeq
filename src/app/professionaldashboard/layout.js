ac13ef96bbe7a3b4ddcda157407c85a291564866
krishna <votivedesigner.krishna@gmail.com>
2025-07-01 14:16:45 +0530

    fix issues

--- src/app/professionaldashboard/layout.js
+++ src/app/professionaldashboard/layout.js
@@ -11,12 +11,12 @@ export default function ClientLayout({ children }) {
                 <section className="flex h-screen bg-gray-50 overflow-hidden">
                      <div
             className={`z-50 ${
-              collapsed ? "w-20" : "w-64"
+              collapsed ? "w-20" : "lg:w-64 w:100"
             } transition-all duration-300`}
           >
                         <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
                     </div>
-                    <div className="flex-1 flex flex-col lg:p-5 p-2">
+                    <div className="flex-1 flex flex-col custom-scrollbar lg:p-5 p-2 overflow-x-hidden">
                         <Navbar wish={"أهلا بك"} location={"موقعك"} url={"watheeq.com/u/username"} username={"أحمد ماضي"} usertype={"محامي"} profileimage={"/watheeq/assets/img/avatar.png"} profilelink={"/professionaldashboard/profile"} />
                         {children}
                     </div>
